a2ce3ef2e45bc0867dccbef664f16ec714bbd8e3
gokulrenga-db <gokul.renganathan@databricks.com>
2024-11-08 16:21:42 +0530

    demo committed

--- setup.py
+++ setup.py
@@ -1,15 +1,21 @@
 # Filename: setup.py
 from setuptools import setup, find_packages
+import sys
+sys.path.append('./src')
 
 import dabdemo
 
 setup(
   name = "dabdemo",
   version = dabdemo.__version__,
-  author = dabdemo.__author__,
   author_email = "gokul.renganathan@databricks.com",
   description = "cicd test",
-  packages = find_packages(include = ["dabdemo"]),
-  entry_points={"group_1": "run=dabdemo.__main__:main"},
+  packages = find_packages(where='./src'),
+  package_dir={'': 'src'},
+  entry_points={
+        "packages": [
+            "main=dabdemo.main:main"
+        ]
+    },
   install_requires = ["setuptools"]
 )
